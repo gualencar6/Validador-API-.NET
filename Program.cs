af55410edc3a1283d6e2764fa4840c9e1bdb6f27
gualencar6 <alencarsgustavo@outlook.com>
2026-05-04 13:15:16 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Controllers
 builder.Services.AddControllers();
 
-// 🔥 ADICIONA ISSO (Swagger)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 🔥 ADICIONA ISSO (Swagger)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
